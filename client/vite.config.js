c8c934ca8494b317ca1dd44819d8b55bc924d2fd
vidhya-balaji <vidhyabalaji.nina@gmail.com>
2025-03-12 01:07:19 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve("E:\\vidhya\\FinalProject\\MERN_Ecommerce\\client", "./src"),
     },
   },
 })
